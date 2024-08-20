4b91bd13d782c8264aa08272b4dca541f4122590
coder-w <phoenixedian@gmail.com>
2024-08-20 06:08:01 +0300

    added background coloor to identify the Army

--- bot battlr/src/components/Army.jsx
+++ bot battlr/src/components/Army.jsx
@@ -8,11 +8,17 @@ useEffect(()=>{
     
     const addedArmy = army.map((arm)=>{
 return (
-    <div  key={arm.id} className="w3-third w3-centre">
+    <div  key={arm.id} className="w3-third w3-centre w3-red">
+                <div>I AM PART OF YOUR ARMY </div>
+
     <div  className="w3-card-4 w3-dark-grey w3-threequarter  w3-center">
+  
     <img  className="w3-image w3-circle" src={arm.avatar_url} alt ="bot" style={{width:'45%'}}/>
     <div className="w3-container w3-centre">
         <p>{arm.name}</p>
+        <p>Health: {arm.health}</p>
+              <p>Damage: {arm.damage}</p>
+              <p>Armor: {arm.armor}</p>
        
     </div>
     </div> 
@@ -26,8 +32,8 @@ return (
 
 
     return (
-      <>
+      <div>
     {armyBots}
-      </>
+      </div>
     );
   }
